35db25408e2802470b532caf1192a98c421a336c
mspathy <mspathy@gmail.com>
2017-07-02 12:46:05 +0530

    Cleanup the server.js

--- weatherapp.js
+++ weatherapp.js
@@ -1,24 +1,15 @@
 var express = require("express");
 var app     = express();
 var path    = require("path");
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-//app.use('/', express.static('js'));
-//app.use('/', express.static('css'));
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-app.get('/weather',function(req,res){
   res.sendFile(path.join(__dirname+'/Weather.html'));
 });
 
-app.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/sitemap.html'));
+
+app.listen(PORT, function () {
+	console.log('Hello from port : ' + PORT);
 });
-
-app.listen(3000);
-
-console.log("Running at Port 3000");
